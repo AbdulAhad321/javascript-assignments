178d5bc62663a3a232acd0fc23397aad0e6d1527
IAbdulAhad0607 <khanabahad999@gmail.com>
2025-10-24 18:22:31 +0500

    please add

---
+++ Problems-solved/app.js
@@ -0,0 +1,100 @@
+// console.log("Hello World!")
+
+
+// Extra Task --> 
+
+// function doNumLe (num1, num2) {
+// // console.log(num1, num2) 
+// if(num1 == num2){
+//     return console.log("both values are equal")
+// }
+// if(num1 < num2){
+//     console.log(num1 , " is greater than" , num2)
+// }else {
+//     console.log(num2, " is greater than" , num1)
+// }
+// }
+
+
+// doNumLe(12, 12)
+
+
+
+// Problem # 1 
+
+// let firstName = "Muhammad"
+// let lastName = "Hasan"
+
+// let address = "main street"
+
+// // reassign ==>
+
+// address = "fulan dhimkhana == >"
+
+// console.log("first name ==> ", firstName)
+// console.log("last name ==> ", lastName)
+// console.log("address ==> ", address)
+
+
+
+
+// Problem # 2
+
+// let street = "2nd street Gulshan"
+// let country = "Pakistan"
+
+
+// let fullMailingAdd = street + " " + country
+
+// console.log("full mailing address ==>",fullMailingAdd)
+
+
+
+// Problem # 3
+
+// let score1 = 33
+// let score2 = 33
+// let score3 = 33
+
+// let totalScore  = score1 + score2 + score3
+
+// console.log(totalScore);
+
+
+// let plates = 20;
+// let people = 7;
+
+// let remainingPlates = plates - people
+
+
+// let message = `There are ${remainingPlates} plates available`
+
+
+
+
+
+// Problem # 4
+
+// let fruits = ["apple", "banana", "mango", "orange", 125]
+// let firstFruit = fruits[0]
+
+// fruits[fruits.length -1] = "grapes"
+
+// console.log("Actual Fruits Array --->",fruits)
+// console.log("First Fruit Variable --->",firstFruit)
+
+
+
+// Problem # 5
+function calculateTotal (subTotal, tax) {
+    return subTotal + tax
+}
+
+let order1= calculateTotal(25, 25);
+let order2=calculateTotal(125, 25);
+let order3= calculateTotal(255, 25);
+
+
+console.log("Order 1-->",order1)
+console.log("Order 2-->",order2)
+console.log("Order 3-->",order3)
